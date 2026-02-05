2526a1b6eec4b52d1442a7a2a88ec4ea97587e3c
2020704 <condori.cristian2020@gmail.com>
2026-02-05 18:48:54 -0400

    Tercer Cambio de Integracion

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,5 +1,8 @@
 import { describe, it, expect } from 'vitest';
 import { sum } from '../src/sum.js';
+import { res } from '../src/resta.js';
+import { mul } from '../src/multiplicacion.js';
+import { div } from '../src/division.js';
 
 describe('sum', () => {
 
@@ -9,5 +12,35 @@ describe('sum', () => {
 
   it('suma mas cero', () => {
     expect(sum(4, 0)).toBe(4);
+  });
+});
+describe('res', () => {
+
+  it('resta dos números', () => {
+    expect(res(7, 5)).toBe(2);
+  });
+
+  it('resta menos cero', () => {
+    expect(res(2, 0)).toBe(2);
+  });
+});
+describe('mul', () => {
+
+  it('multiplica dos números', () => {
+    expect(mul(6, 3)).toBe(18);
+  });
+
+  it('multiplica por  cero', () => {
+    expect(mul(9, 0)).toBe(0);
+  });
+});
+describe('div', () => {
+
+  it('divide dos números', () => {
+    expect(div(8, 2)).toBe(4);
+  });
+
+  it('divide entre cero', () => {
+    expect(div(4, 0)).toBe(Infinity);
   });
 });
